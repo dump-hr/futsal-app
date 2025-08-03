175d3183e833e1f99e0dfbecf7d58e8c54ae199d
tsiklic1 <toma.siklic@gmail.com>
2025-08-03 12:17:31 +0200

    added prisma module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ServeStaticModule.forRoot({
-    rootPath: join(__dirname, '../..', 'web', 'dist')
-  })],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'web', 'dist'),
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
