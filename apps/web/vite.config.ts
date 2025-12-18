0524a2ac9b2f5122e005ddee76ce46962330f503
ninom5 <nmilas00@fesb.hr>
2025-12-18 22:07:16 +0100

    fix?

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths({
+      projects: [path.resolve(__dirname, 'tsconfig.json')],
+    }),
+  ],
   server: {
     proxy: {
       '/api': {
