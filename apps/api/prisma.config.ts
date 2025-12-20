90d1a017bb33d27773fbb6b0f6906e6c53723b5c
ninom5 <nmilas00@fesb.hr>
2025-12-20 15:16:49 +0100

    Change configuration to use prisma 7ž

---
+++ apps/api/prisma.config.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import { defineConfig, env } from 'prisma/config';
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: {
+    url: 'postgres://dumpovac:dump1234@localhost:3001/futsal-db',
+  },
+});
