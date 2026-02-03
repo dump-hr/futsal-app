d67b2c634db67043ab1fede8aeb0af46550d2bf8
tsiklic1 <toma.siklic@gmail.com>
2026-02-03 18:59:02 +0100

    Add fonts

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,3 +1,4 @@
+import './App.scss';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Toaster } from 'react-hot-toast';
 import { Router } from './Router';
--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -1,7 +1,12 @@
+import c from './HomePage.module.scss';
+
 export const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1 className={c.a}>Title 1</h1>
+      <h1 className={c.b}>Title 1</h1>
+      <h1 className={c.c}>Title 1</h1>
     </div>
   );
 };
+// export default HomePage;
