7e4f13e37fd10dec794fda07abb931fea7d37d3b
tsiklic1 <toma.siklic@gmail.com>
2026-02-06 21:41:35 +0100

    Add ButtonSmall component

---
+++ apps/web/src/components/ButtonSmall/index.ts
@@ -0,0 +1,7 @@
+import { BackgroundColor } from './ backgroundColor';
+import ButtonSmall from './ButtonSmall';
+import type { BackgroundColor as BackgroundColorType } from './ backgroundColor';
+
+export { BackgroundColor, BackgroundColorType };
+
+export default ButtonSmall;
