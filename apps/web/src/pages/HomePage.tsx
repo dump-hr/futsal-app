be2b432b3d49bd0cf6586191231e1dbc39e430b8
tsiklic1 <toma.siklic@gmail.com>
2026-02-06 21:42:04 +0100

    Add showcase SmallButton and fonts

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -1,11 +1,30 @@
+import ButtonSmall, { BackgroundColor } from '../components/ButtonSmall';
 import c from './HomePage.module.scss';
+import trashCanSvg from '../assets/icons/trash-can-gray.svg';
+import plusSvg from '../assets/icons/plus-gray.svg';
+import playSvg from '../assets/icons/play-black.svg';
 
 export const HomePage = () => {
   return (
-    <div>
+    <div style={{ background: 'black' }}>
       <h1 className={c.a}>Title 1</h1>
       <h1 className={c.b}>Title 1</h1>
       <h1 className={c.c}>Title 1</h1>
+      <ButtonSmall
+        iconSrc={trashCanSvg}
+        hasBorder
+        backgroundColor={BackgroundColor.Lime}
+      />
+      <ButtonSmall
+        iconSrc={plusSvg}
+        width={40}
+        backgroundColor={BackgroundColor.Red}
+      />
+      <ButtonSmall
+        iconSrc={playSvg}
+        width={40}
+        backgroundColor={BackgroundColor.White}
+      />
     </div>
   );
 };
