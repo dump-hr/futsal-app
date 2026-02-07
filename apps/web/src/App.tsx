92ddbe48dce825916f1b692a0882ad864ababef8
tsiklic1 <toma.siklic@gmail.com>
2026-02-07 20:27:02 +0100

    Move fonts to styles folder

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,4 +1,6 @@
+import '@/styles/_fonts.scss';
 import './App.scss';
+
 import { ErrorBoundary } from 'react-error-boundary';
 import { Toaster } from 'react-hot-toast';
 import { Router } from './Router';
