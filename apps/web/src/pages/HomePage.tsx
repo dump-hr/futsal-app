89bd12fe8db9d278def507c28ebd583f89e23898
lukazuljevic <luka.zuljo@gmail.com>
2026-02-10 20:12:37 +0100

    Absoulte import

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import ButtonSmall, { BackgroundColor } from '../components/ButtonSmall';
-import { Button, ModalConfirmation } from '../components';
+import { Button, ModalConfirmation } from '@components/index';
 import c from './HomePage.module.scss';
 import trashCanSvg from '@assets/icons/trash-can-gray.svg';
 import plusSvg from '@assets/icons/plus-gray.svg';
