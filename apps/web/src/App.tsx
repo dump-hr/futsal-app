c34d656de498191d5250518b5eed1dba1dcd6a74
ninom5 <nmilas00@fesb.hr>
2026-02-10 19:08:15 +0100

    clean up

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,9 +1,9 @@
 import '@/styles/_fonts.scss';
+import './App.scss';
 
 import { ErrorBoundary } from 'react-error-boundary';
 import { Toaster } from 'react-hot-toast';
 import { Router } from './Router';
-import './App.scss';
 
 export const App = () => {
   return (
