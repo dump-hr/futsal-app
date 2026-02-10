00b372add933eb00de575778a3c70dbc1ff1de3f
ninom5 <nmilas00@fesb.hr>
2026-02-10 19:14:23 +0100

    fix lint

--- apps/web/src/layouts/BackgrounLayout/BackgroundLayout.tsx
+++ apps/web/src/layouts/BackgrounLayout/BackgroundLayout.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react';
 import BackgroundImage from '@assets/logo_mlnm.png';
 import c from './BackgroundLayout.module.scss';
 
-export const BackgroundLayout = ({ children }: PropsWithChildren<{}>) => {
+export const BackgroundLayout = ({ children }: PropsWithChildren) => {
   return (
     <div
       className={c.layout}
