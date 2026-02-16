f402552106d56579cffa05b66a4769caed5a0381
dbaric99 <damjana.baric1999@gmail.com>
2026-02-16 01:19:25 +0100

    Added component files

---
+++ apps/web/src/components/TeamInfo/TeamInfo.tsx
@@ -0,0 +1,9 @@
+import c from './TeamInfo.module.scss';
+
+export const TeamInfo: React.FC = () => {
+  return (
+    <div className={c.team}>
+      <div className={c.teamName}></div>
+    </div>
+  );
+};
