1335f31fb4b7460ded2fca30cc09fb73f6b37bcc
lukazuljevic <luka.zuljo@gmail.com>
2026-02-23 22:44:57 +0100

    Change to match prisma v7 change

--- apps/api/src/match/match.module.ts
+++ apps/api/src/match/match.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MatchController } from './match.controller';
 import { MatchService } from './match.service';
-import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [MatchController],
-  providers: [PrismaService, MatchService],
+  providers: [MatchService],
 })
 export class MatchModule {}
--- apps/api/src/match/match.service.ts
+++ apps/api/src/match/match.service.ts
@@ -1,13 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { prisma } from '../../lib/prisma';
 import { MatchDto } from '@futsal-app/types';
 
 @Injectable()
 export class MatchService {
-  constructor(private readonly prisma: PrismaService) {}
-
   async getById(id: number): Promise<MatchDto> {
-    const match = await this.prisma.match.findUnique({
+    const match = await prisma.match.findUnique({
       where: { id },
       include: {
         homeTeam: { select: { id: true, name: true, logoUrl: true } },
