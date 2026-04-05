fd1ec5b5773a4b017cf465b529efd8b990ba4a87
ninom5 <nmilas00@fesb.hr>
2026-04-05 16:40:56 +0200

    fix lint

--- apps/web/src/components/ModalTournamentHistory/ModalTournamentHistory.tsx
+++ apps/web/src/components/ModalTournamentHistory/ModalTournamentHistory.tsx
@@ -22,7 +22,10 @@ const ModalTournamentHistory: React.FC<ModalTournamentHistoryProps> = ({
   const toggleMark = (id: number) => {
     setMarkedForDeletion((prev) => {
       const next = new Set(prev);
-      next.has(id) ? next.delete(id) : next.add(id);
+
+      if (next.has(id)) next.delete(id);
+      else next.add(id);
+
       return next;
     });
   };
