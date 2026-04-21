254bb2f71f4de4a977dc437cc3e0652ac0726d8e
ninom5 <nmilas00@fesb.hr>
2026-04-21 19:02:01 +0200

    revert router

--- apps/web/src/Router.tsx
+++ apps/web/src/Router.tsx
@@ -2,17 +2,20 @@ import { Route, Switch } from 'wouter';
 import { routes } from './routes/index';
 import { BackgroundLayout, NavbarLayout } from '@layouts/index';
 import { HomePage, LoginPage, StartingPage } from '@pages/index';
+import { ProtectedRoute } from './components';
 
 export const Router = () => {
   return (
     <Switch>
       <Route path={routes.LOGIN} component={LoginPage} />
-      <NavbarLayout>
-        <BackgroundLayout>
-          <Route path={routes.ADMIN} component={HomePage} />
-          <Route path={routes.STARTING} component={StartingPage} />
-        </BackgroundLayout>
-      </NavbarLayout>
+      <ProtectedRoute>
+        <NavbarLayout>
+          <BackgroundLayout>
+            <Route path={routes.ADMIN} component={HomePage} />
+            <Route path={routes.STARTING} component={StartingPage} />
+          </BackgroundLayout>
+        </NavbarLayout>
+      </ProtectedRoute>
     </Switch>
   );
 };
