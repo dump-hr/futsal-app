07006d45d483c39ad09968a660fe8dfa8a920a4b
ninom5 <nmilas00@fesb.hr>
2026-04-25 13:47:58 +0200

    revert router

--- apps/web/src/Router.tsx
+++ apps/web/src/Router.tsx
@@ -10,21 +10,24 @@ import {
   MatchesPage,
   GroupsPage,
 } from '@pages/index';
+import { ProtectedRoute } from './components';
 
 export const Router = () => {
   return (
     <Switch>
       <Route path={routes.LOGIN} component={LoginPage} />
-      <NavbarLayout>
-        <BackgroundLayout>
-          <Route path={routes.ADMIN} component={HomePage} />
-          <Route path={routes.ADMIN_HOME} component={StartingPage} />
-          <Route path={routes.TEAM_DETAIL} component={TeamDetailPage} />
-          <Route path={routes.TEAMS} component={TeamsPage} />
-          <Route path={routes.MATCHES} component={MatchesPage} />
-          <Route path={routes.GROUPS} component={GroupsPage} />
-        </BackgroundLayout>
-      </NavbarLayout>
+      <ProtectedRoute>
+        <NavbarLayout>
+          <BackgroundLayout>
+            <Route path={routes.ADMIN} component={HomePage} />
+            <Route path={routes.ADMIN_HOME} component={StartingPage} />
+            <Route path={routes.TEAM_DETAIL} component={TeamDetailPage} />
+            <Route path={routes.TEAMS} component={TeamsPage} />
+            <Route path={routes.MATCHES} component={MatchesPage} />
+            <Route path={routes.GROUPS} component={GroupsPage} />
+          </BackgroundLayout>
+        </NavbarLayout>
+      </ProtectedRoute>
     </Switch>
   );
 };
