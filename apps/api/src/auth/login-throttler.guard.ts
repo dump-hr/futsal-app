11dc938ace1abfd94275a0a0fb965a00b38774b5
lukazuljevic <luka.zuljo@gmail.com>
2026-04-26 16:09:56 +0200

    Fix lint

--- apps/api/src/auth/login-throttler.guard.ts
+++ apps/api/src/auth/login-throttler.guard.ts
@@ -3,7 +3,9 @@ import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Injectable()
 export class LoginThrottlerGuard extends ThrottlerGuard {
-  protected async getErrorMessage(): Promise<string> {
-    return 'Previše pokušaja prijave. Pokušajte ponovno za minutu.';
+  protected getErrorMessage(): Promise<string> {
+    return Promise.resolve(
+      'Previše pokušaja prijave. Pokušajte ponovno za minutu.',
+    );
   }
 }
