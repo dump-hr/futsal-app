ba9909d6c341573e3ac96d123e4e7beb08cff8e0
lukazuljevic <luka.zuljo@gmail.com>
2026-07-13 19:11:19 +0200

    Change the domain name

--- infrastructure/terraform/live/production/compute/main.tf
+++ infrastructure/terraform/live/production/compute/main.tf
@@ -54,7 +54,7 @@ module "web" {
   subnets                   = data.aws_subnets.public_subnets.ids
   security_groups           = data.aws_security_groups.public_sg.ids
   ssh_public_key            = file("../../../../ssh-keys/production.pub")
-  website_domain            = "malonogometni.dump.hr"
+  website_domain            = "turnir.dump.hr"
   cloudflare_zone_id        = data.cloudflare_zone.dump_hr.id
 
   tags = {
