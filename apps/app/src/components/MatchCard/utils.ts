0aee5ff683807e7911ca7c0762d720b35816bca9
tsiklic1 <100917481+tsiklic1@users.noreply.github.com>
2026-09-03 19:31:38 +0200

    add shootout score to matchcard

--- apps/app/src/components/MatchCard/utils.ts
+++ apps/app/src/components/MatchCard/utils.ts
@@ -38,9 +38,16 @@ export const getMatchCardLargeView = (
 
 export const getMatchCardView = (match: MatchDto, elapsedMinutes?: number) => {
   const view = getMatchView(match, elapsedMinutes);
+  const homeShootoutGoals = match.homeShootoutGoals ?? 0;
+  const awayShootoutGoals = match.awayShootoutGoals ?? 0;
+  const hasShootoutScore = homeShootoutGoals > 0 || awayShootoutGoals > 0;
 
   return {
     ...view,
     score: view.isUpcoming ? null : `${match.homeGoals} - ${match.awayGoals}`,
+    shootoutScore:
+      !view.isUpcoming && hasShootoutScore
+        ? `(${homeShootoutGoals} - ${awayShootoutGoals})`
+        : null,
   };
 };
